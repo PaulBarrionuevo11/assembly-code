9e4040e6b6c402f1f9ef75334cccc6b57849076f
Paul Cuadros <paul.barrionuevo.11@gmail.com>
2025-05-19 18:25:07 -0700

    Update quiz4.asm

--- class 116/quiz4.asm
+++ class 116/quiz4.asm
@@ -2,13 +2,11 @@ section .text
 global is_palindrome
 
 is_palindrome:
-
     xor rdx, rdx          
     mov rcx, rsi
     dec rcx               
 
 .check_loop:
-
     ; Compare characters
     mov al, [rdi + rdx]   ; Char at start
     cmp al, [rdi + rcx]   ; Char at end
